667ffe2cd91d8072435d3bf95eb2b3a3a44eaf83
Himanshu2297 <47809296+Himanshu2297@users.noreply.github.com>
2022-09-06 12:52:40 +0530

    2 files modified

--- src/app/services/car-search.service.ts
+++ src/app/services/car-search.service.ts
@@ -1,135 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { catchError } from 'rxjs/operators';
-
+import { car_list } from '../car-itineraries';
 @Injectable({
   providedIn: 'root',
 })
 export class CarSearchService {
   searchDetails = {};
   sortDetails: any;
-  vehicleDetails: any = {
-    carMockItineraries: {
-      currency: 'USD',
-      CarItineraries: [
-        {
-          id: '8ITI1R2FVI61116-2304.1',
-          unlimitedMileage: true,
-          cancellationDetails: {
-            isCancellationFree: true,
-            freeCancellationEndDate: '2020-06-29T10:29:59',
-          },
-          fare: {
-            totalAmount: 604,
-            perDay: 36,
-          },
-          vehicle: {
-            name: 'Group A - Kia Rio or similar',
-            type: 'A class',
-            image:
-              'https://www.avis.com/content/dam/cars/l/2021/toyota/2021-toyota-camry-se-sedan-black.png',
-            specifications: {
-              baggageCapacity: '1',
-              passengerCapacity: '5',
-            },
-          },
-        },
-        {
-          id: '8ITI1R2FVI61116-2304.2',
-          unlimitedMileage: true,
-          cancellationDetails: {
-            isCancellationFree: true,
-            freeCancellationEndDate: '2020-06-29T10:29:59',
-          },
-          fare: {
-            totalAmount: 636,
-            perDay: 37,
-          },
-          vehicle: {
-            name: 'Group B - Ford Focus or similar',
-            type: 'C Class',
-            image:
-              'http://www.avis.com/car-rental/images/global/en/rentersguide/vehicle_guide/2016-ford-focus-se-sedan-black.png',
-            specifications: {
-              baggageCapacity: '4',
-              passengerCapacity: '5',
-            },
-          },
-        },
-        {
-          id: '8ITI1R2FVI61116-2304.3',
-          unlimitedMileage: true,
-          cancellationDetails: {
-            isCancellationFree: true,
-            freeCancellationEndDate: '2020-06-29T10:29:59',
-          },
-          fare: {
-            totalAmount: 636,
-            perDay: 37,
-          },
-          vehicle: {
-            name: 'Group B - Kia Soul or similar',
-            type: 'D Class',
-            image:
-              'https://www.avis.com/content/dam/cars/l/2021/kia/2021-kia-soul-s-5door-hatchback-black.png',
-            specifications: {
-              baggageCapacity: '2',
-              passengerCapacity: '5',
-            },
-          },
-        },
-        {
-          id: '8ITI1R2FVI61116-2304.4',
-          unlimitedMileage: true,
-          cancellationDetails: {
-            isCancellationFree: true,
-            freeCancellationEndDate: '2020-06-29T10:29:59',
-          },
-          fare: {
-            totalAmount: 763,
-            perDay: 42,
-          },
-          vehicle: {
-            name: 'Group C - Toyata Corolla or similar',
-            type: 'B Class',
-            image:
-              'https://www.avis.com/content/dam/cars/l/2020/toyota/2020-toyota-corolla-le-sedan-grey.png',
-            specifications: {
-              baggageCapacity: '5',
-              passengerCapacity: '5',
-            },
-          },
-        },
-        {
-          id: '8ITI1R2FVI61116-2304.5',
-          unlimitedMileage: true,
-          cancellationDetails: {
-            isCancellationFree: true,
-            freeCancellationEndDate: '2020-06-29T10:29:59',
-          },
-          fare: {
-            totalAmount: 746,
-            perDay: 41,
-          },
-          vehicle: {
-            name: 'Group C - Volkswagen Jetta or similar',
-            type: 'C Class',
-            image:
-              'https://www.avis.com/content/dam/cars/l/2021/volkswagen/2021-volkswagen-jetta-s-sedan-silver.png',
-            specifications: {
-              baggageCapacity: '2',
-              passengerCapacity: '5',
-            },
-          },
-        },
-      ],
-      paging: {
-        pageNo: 1,
-        pageSize: 10,
-      },
-    },
-  };
+  vehicleDetails: any = car_list;
 
   sortValues = [
     {
@@ -160,21 +37,21 @@ export class CarSearchService {
 
   getVehicleDetails() {
     if (this.sortDetails && this.sortDetails.sortType == '104') {
-      this.vehicleDetails.carMockItineraries.CarItineraries.sort((a, b) => {
+      this.vehicleDetails.CarItineraries.sort((a, b) => {
         return a.fare.perDay - b.fare.perDay;
       });
       console.log('sport', this.sortDetails.sortType);
     }
 
     if (this.sortDetails && this.sortDetails.sortType == '105') {
-      this.vehicleDetails.carMockItineraries.CarItineraries.sort((a, b) => {
+      this.vehicleDetails.CarItineraries.sort((a, b) => {
         return b.fare.perDay - a.fare.perDay;
       });
       console.log('sport', this.sortDetails.sortType);
     }
 
     if (this.sortDetails && this.sortDetails.sortType == '106') {
-      this.vehicleDetails.carMockItineraries.CarItineraries.sort((a, b) => {
+      this.vehicleDetails.CarItineraries.sort((a, b) => {
         let fa = a.vehicle.name.toLowerCase(),
           fb = b.vehicle.name.toLowerCase();
 
@@ -191,7 +68,7 @@ export class CarSearchService {
     }
 
     if (this.sortDetails && this.sortDetails.sortType == '107') {
-      this.vehicleDetails.carMockItineraries.CarItineraries.sort((a, b) => {
+      this.vehicleDetails.CarItineraries.sort((a, b) => {
         let fb = a.vehicle.name.toLowerCase(),
           fa = b.vehicle.name.toLowerCase();
 
@@ -207,7 +84,7 @@ export class CarSearchService {
       console.log('sport', this.sortDetails.sortType);
     }
     if (this.sortDetails && this.sortDetails.sortType == '108') {
-      this.vehicleDetails.carMockItineraries.CarItineraries.sort((a, b) => {
+      this.vehicleDetails.CarItineraries.sort((a, b) => {
         let fa = a.vehicle.type.toLowerCase(),
           fb = b.vehicle.type.toLowerCase();
 
@@ -224,7 +101,7 @@ export class CarSearchService {
     }
 
     if (this.sortDetails && this.sortDetails.sortType == '109') {
-      this.vehicleDetails.carMockItineraries.CarItineraries.sort((a, b) => {
+      this.vehicleDetails.CarItineraries.sort((a, b) => {
         let fb = a.vehicle.type.toLowerCase(),
           fa = b.vehicle.type.toLowerCase();
 
